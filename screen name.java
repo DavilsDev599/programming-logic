25718ec3aab1347213cffde2c04c875b2a0ca3e3
Davi Laurindo <davi.silva515@aluno.ce.gov.br>
2024-11-04 21:33:45 -0300

    Atualizar o
    This code will ask for the user name and will be typed on the screen

---
+++ screen name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]  args ){
+        Scanner leia=new Scanner(System.in);
+        
+       System.out.println("digite seu nome");
+       String nome=leia.next();
+       
+       System.out.println("Seu nome é"+nome);
+       
+       }
+       
+    }
